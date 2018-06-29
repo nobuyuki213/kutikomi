77cbfb8fdb545b56a8e6c67e83c6e2ac2338e286
no-bee <nobu0213yuki@gimail.com>
2018-06-29 11:29:43 +0900

    tweak

--- resources/views/places/review.blade.php
+++ resources/views/places/review.blade.php
@@ -19,7 +19,7 @@
 	{!! Form::open(['route' => 'places.review', 'method' => 'get']) !!}
 	<div class="form-group row mx-auto">
 		{!! Form::text('keyword', empty($keyword) ? old('keyword') : $keyword, ['class' => 'form-control form-control-lg offset-sm-1 col-sm-8 ml-auto my-1']) !!}
-		{!! Form::button('<i class="fas fa-search fa-sm"> 検索</i>', ['class' => 'btn btn-secondary  btn-lg col-sm-2 mr-auto m-1', 'type' => 'submit']) !!}
+		{!! Form::button('<i class="fas fa-search fa-sm"></i> 検索', ['class' => 'btn btn-secondary  btn-lg col-sm-2 mr-auto m-1', 'type' => 'submit']) !!}
 	</div>
 	{!! Form::close()!!}
 	<div>
