64f1e082215e2f136668c9ad1474046b467f6b86
no-bee <nobu0213yuki@gimail.com>
2018-07-23 20:03:26 +0900

    AppServiceProvider correction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
         //
         \Schema::defaultStringLength(191);
 
-        if (\App::environment('prodauction')) {
+        if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
     }
